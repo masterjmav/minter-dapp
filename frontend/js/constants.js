c4dac74fe594fa678a5b704cc9ff3eba35d66db2
masterjmav <masterjmav@gmail.com>
2022-04-08 21:13:13 -0400

    space

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Surfers NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Surfers NFT Project is a unique nft experiment made by Masterjmav. Come visit my site! <a href="https://www.nft-tribune.com"  target="_blank" rel="noopener noreferrer">NFT Tribune</a> Main Website.';
+const welcome_p = 'The Surfers NFT Project is a unique nft experiment made by Masterjmav. Come visit my site!       <a href="https://www.nft-tribune.com"  target="_blank" rel="noopener noreferrer">NFT Tribune</a> Main Website.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
