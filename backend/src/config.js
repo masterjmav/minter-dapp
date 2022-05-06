018a5897a53cbdd2ed3a2be2161cfeb2c73a915a
masterjmav <masterjmav@gmail.com>
2022-05-05 21:14:18 -0400

    pub2

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const MINT_PRICE = 25; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC.
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-06-19T22:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-06-19T22:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-06-12T22:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
