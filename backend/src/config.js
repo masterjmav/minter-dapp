856145777acf3bdcd33f815634bd1606846a7982
masterjmav <masterjmav@gmail.com>
2022-06-09 21:14:01 -0400

    new

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,10 +61,10 @@ const MINT_PRICE = 25; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC.
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-06-23T23:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-06-23T23:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-06-12T22:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-06-12T22:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x05Ba13eA7E7DBCc2BDA8e4b9C2141cBC2aE309C8"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
